ea7aa5b16ee28d2e3549286e2c8a1305cc055967
@maiow <emmaria@yandex.ru>
2025-03-15 20:16:32 +0300

    add action and bash script

---
+++ extract-expose-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$EXPOSE_CODE" == 'true' ]; then
+    ANDROID_VERSION_CODE=$(grep "versionCode" app/build.gradle.kts | sed "s/.*versionCode\\s*=\\s*\\([0-9]*\\).*/\\1/")
+    echo "ANDROID_VERSION_CODE=$ANDROID_VERSION_CODE" >> $GITHUB_ENV
+fi
+
+if [ "$EXPOSE_NAME" == 'true' ]; then
+    ANDROID_VERSION_NAME=$(grep "versionName" app/build.gradle.kts | sed "s/.*versionName\\s*=\\s*'\\(.*\\)'.*/\\1/" | tr -d '[:space:]' | sed 's/"//g' | sed 's/versionName=//')
+    echo "ANDROID_VERSION_NAME=$ANDROID_VERSION_NAME" >> $GITHUB_ENV
+fi
