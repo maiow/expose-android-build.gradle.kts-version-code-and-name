78375a92c63e8ff51d7686ed154aceb1ceaba69b
@maiow <emmaria@yandex.ru>
2025-03-15 21:06:22 +0300

    env to file

--- extract-expose-version.sh
+++ extract-expose-version.sh
@@ -2,10 +2,10 @@
 
 if [ "$EXPOSE_CODE" == 'true' ]; then
     ANDROID_VERSION_CODE=$(grep "versionCode" app/build.gradle.kts | sed "s/.*versionCode\\s*=\\s*\\([0-9]*\\).*/\\1/")
-    echo "ANDROID_VERSION_CODE=$ANDROID_VERSION_CODE" >> $GITHUB_ENV
+    echo "ANDROID_VERSION_CODE=$ANDROID_VERSION_CODE" >> extracted_values.txt
 fi
 
 if [ "$EXPOSE_NAME" == 'true' ]; then
     ANDROID_VERSION_NAME=$(grep "versionName" app/build.gradle.kts | sed "s/.*versionName\\s*=\\s*'\\(.*\\)'.*/\\1/" | tr -d '[:space:]' | sed 's/"//g' | sed 's/versionName=//')
-    echo "ANDROID_VERSION_NAME=$ANDROID_VERSION_NAME" >> $GITHUB_ENV
+    echo "ANDROID_VERSION_NAME=$ANDROID_VERSION_NAME" >> extracted_values.txt
 fi
