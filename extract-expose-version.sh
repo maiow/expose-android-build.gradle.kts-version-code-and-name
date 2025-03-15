63624d11b54596847eebb22be01db2f75fe0ce67
@maiow <emmaria@yandex.ru>
2025-03-15 22:13:23 +0300

    addl file to pass vars

--- extract-expose-version.sh
+++ extract-expose-version.sh
@@ -12,16 +12,18 @@ fi
 # Extract Android version code
 if [ "$EXPOSE_CODE" == 'true' ]; then
     ANDROID_VERSION_CODE=$(grep "versionCode" app/build.gradle.kts | sed "s/.*versionCode\\s*=\\s*\\([0-9]*\\).*/\\1/")
-    export ANDROID_VERSION_CODE
     echo "ANDROID_VERSION_CODE extracted: $ANDROID_VERSION_CODE"
 fi
 
 # Extract Android version name
 if [ "$EXPOSE_NAME" == 'true' ]; then
     ANDROID_VERSION_NAME=$(grep "versionName" app/build.gradle.kts | sed "s/.*versionName\\s*=\\s*'\\(.*\\)'.*/\\1/" | tr -d '[:space:]' | sed 's/"//g' | sed 's/versionName=//')
-    export ANDROID_VERSION_NAME
     echo "ANDROID_VERSION_NAME extracted: $ANDROID_VERSION_NAME"
 fi
+
+# Write the extracted values to a temporary file
+echo "ANDROID_VERSION_CODE=$ANDROID_VERSION_CODE" > extracted_values.txt
+echo "ANDROID_VERSION_NAME=$ANDROID_VERSION_NAME" >> extracted_values.txt
 
 # Debugging output to indicate script completion
 echo "Script execution complete."
