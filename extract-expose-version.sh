dd3b35a610b8a95aa75624fb7575933fd823b1d4
@maiow <emmaria@yandex.ru>
2025-03-15 22:16:59 +0300

    debug echo for grep

--- extract-expose-version.sh
+++ extract-expose-version.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Debugging output to check script execution
-echo "Script is being executed..."
-
-# Check if the build.gradle.kts file exists
-if [ ! -f "app/build.gradle.kts" ]; then
-    echo "Error: app/build.gradle.kts file not found."
+# Extract Android version code
+ANDROID_VERSION_CODE=$(grep "versionCode" app/build.gradle.kts | sed "s/.*versionCode\\s*=\\s*\\([0-9]*\\).*/\\1/")
+if [ -z "$ANDROID_VERSION_CODE" ]; then
+    echo "Error: Unable to extract version code."
     exit 1
 fi
 
-# Extract Android version code
-if [ "$EXPOSE_CODE" == 'true' ]; then
-    ANDROID_VERSION_CODE=$(grep "versionCode" app/build.gradle.kts | sed "s/.*versionCode\\s*=\\s*\\([0-9]*\\).*/\\1/")
-    echo "ANDROID_VERSION_CODE extracted: $ANDROID_VERSION_CODE"
-fi
-
 # Extract Android version name
-if [ "$EXPOSE_NAME" == 'true' ]; then
-    ANDROID_VERSION_NAME=$(grep "versionName" app/build.gradle.kts | sed "s/.*versionName\\s*=\\s*'\\(.*\\)'.*/\\1/" | tr -d '[:space:]' | sed 's/"//g' | sed 's/versionName=//')
-    echo "ANDROID_VERSION_NAME extracted: $ANDROID_VERSION_NAME"
+ANDROID_VERSION_NAME=$(grep "versionName" app/build.gradle.kts | sed "s/.*versionName\\s*=\\s*'\\(.*\\)'.*/\\1/")
+if [ -z "$ANDROID_VERSION_NAME" ]; then
+    echo "Error: Unable to extract version name."
+    exit 1
 fi
 
 # Write the extracted values to a temporary file
