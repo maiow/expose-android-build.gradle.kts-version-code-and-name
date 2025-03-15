834a03864c32651e4c20a530ab70d5cdafa1c68e
@maiow <emmaria@yandex.ru>
2025-03-15 21:21:55 +0300

    add debug echoing

--- extract-expose-version.sh
+++ extract-expose-version.sh
@@ -4,10 +4,12 @@
 if [ "$EXPOSE_CODE" == 'true' ]; then
     ANDROID_VERSION_CODE=$(grep "versionCode" app/build.gradle.kts | sed "s/.*versionCode\\s*=\\s*\\([0-9]*\\).*/\\1/")
     export ANDROID_VERSION_CODE
+    echo "ANDROID_VERSION_CODE extracted: $ANDROID_VERSION_CODE"
 fi
 
 # Extract Android version name
 if [ "$EXPOSE_NAME" == 'true' ]; then
     ANDROID_VERSION_NAME=$(grep "versionName" app/build.gradle.kts | sed "s/.*versionName\\s*=\\s*'\\(.*\\)'.*/\\1/" | tr -d '[:space:]' | sed 's/"//g' | sed 's/versionName=//')
     export ANDROID_VERSION_NAME
+    echo "ANDROID_VERSION_NAME extracted: $ANDROID_VERSION_NAME"
 fi
